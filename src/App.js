bd8fa5a30339282596dade693585fcf8438e8964
Your Name <zukokalilov@gmail.com>
2022-05-18 20:35:18 +0600

    homework

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './componet/TodoFrom';
+
+function App() {
+  return (
+    <div className="App"> 
+
+    <TodoForm />
+      
+    </div>
+  );
+}
+
+export default App;
